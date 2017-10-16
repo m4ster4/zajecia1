16c700fe3dca33d522e38606134fb49ed99e4e1f
student <student@S006-5QQ185J-U>
2017-10-16 13:22:52 +0200

    13:22

--- zajecia_01/labirynt/py/__init__.py
+++ zajecia_01/labirynt/py/__init__.py
@@ -1,1 +1,25 @@
-print 'hello world'
+
+with open("labirynt0.txt", "r") as f_in:
+    lines = filter(None, (line.rstrip() for line in f_in))
+        
+
+# print labirynt
+
+start = '@'
+end = '$'
+
+# print lines
+# print lines[0]
+height,width= lines[0].split(' ')
+
+# print height,width
+
+for line in lines:
+    for c in line:
+        if c == start:
+             'start'
+    print line
+
+
+
+
